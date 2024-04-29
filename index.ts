866b949b9541ec763b908780856c821a3cebb4e1
Aykut Alpgiray Ateş <alp.ates@mentimeter.com>
2024-04-29 10:45:13 +0200

    Logs are removed

--- index.ts
+++ index.ts
@@ -29,7 +29,18 @@ const program = Effect.gen(function* (_) {
   const [scriptName, ...restArgs] = yield* _(
     pipe(
       Effect.fromNullable(Bun.argv),
-      Effect.map((v) => v.slice(2)),
+      Effect.tryMap({
+        try: (v) => {
+          const args = v.slice(2);
+
+          if (args.length === 0) {
+            throw new Error("No script name provided");
+          }
+
+          return args;
+        },
+        catch: () => Effect.fail(new Error("No script name provided")),
+      }),
       Effect.orElse(() => Effect.succeed(["dev"])),
     ),
   );
